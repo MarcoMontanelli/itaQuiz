abe7e11b9da89539dd26001ceefabda894d9747d
MarcoMontanelli <101709469+MarcoMontanelli@users.noreply.github.com>
2024-05-22 13:21:21 +0200

    erdtfyui

--- src/page/QuizCard.jsx
+++ src/page/QuizCard.jsx
@@ -1,30 +1,70 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 
-const QuestionCard = ({ question, options, image, selectedOption, onSelectOption, onNext, onPrev }) => {
+const QuestionCard = ({ question, options, media, mediaType, selectedOption, onSelectOption, onNext, onPrev }) => {
+  const renderMedia = () => {
+    switch (mediaType) {
+      case 'image':
+        return (
+          <motion.img
+            src={media}
+            alt="Question media"
+            className="w-full h-64 object-cover mb-4 rounded"
+            initial={{ opacity: 0, scale: 0.9 }}
+            animate={{ opacity: 1, scale: 1 }}
+          />
+        );
+      case 'video':
+        return (
+          <motion.video
+            controls
+            src={media}
+            className="w-full h-64 object-cover mb-4 rounded"
+            initial={{ opacity: 0, scale: 0.9 }}
+            animate={{ opacity: 1, scale: 1 }}
+          />
+        );
+      case 'youtube':
+        return (
+          <motion.iframe
+            src={media}
+            className="w-full h-64 object-cover mb-4 rounded"
+            initial={{ opacity: 0, scale: 0.9 }}
+            animate={{ opacity: 1, scale: 1 }}
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></motion.iframe>
+        );
+      case 'text':
+        return (
+          <motion.div
+            className="w-full h-64 bg-gray-800 text-white text-bold p-4 rounded mb-4"
+            initial={{ opacity: 0, scale: 0.9 }}
+            animate={{ opacity: 1, scale: 1 }}
+          >
+            {media}
+          </motion.div>
+        );
+      default:
+        return null;
+    }
+  };
+
   return (
     <motion.div
       initial={{ opacity: 0, x: 100 }}
       animate={{ opacity: 1, x: 0 }}
       exit={{ opacity: 0, x: -100 }}
-      className="bg-slate-950 border border-gray-700 rounded-lg p-4 mb-4 w-full max-w-2xl mx-auto"
+      className="bg-slate-950 border border-gray-700 rounded-lg p-4 mb-4 w-full max-w-3xl mx-auto"
     >
       <div className="flex justify-between items-center mb-4">
         <h2 className="text-2xl font-extrabold">Question</h2>
         <button className="bg-gray-700 text-white font-extrabold rounded-lg p-2 "> ? </button>
       </div>
-      {image && (
-        <motion.img
-          src={image}
-          alt="Question media"
-          className="w-full h-64 object-cover mb-4 rounded"
-          initial={{ opacity: 0, scale: 0.9 }}
-          animate={{ opacity: 1, scale: 1 }}
-        />
-      )}
-      <p className="mb-4 font-bold">{question}</p>
+      {renderMedia()}
+      <p className="mb-4 text-2xl font-bold">{question}</p>
       <div className="mb-4">
-        <h3 className="font-bold text-xl mb-2">Choose an answer carefully:</h3>
+        <h3 className="font-bold  mb-2">Choose an answer carefully:</h3>
         {options.map((option, index) => (
           <motion.button
             key={index}
--- src/questions/Questions.jsx
+++ src/questions/Questions.jsx
@@ -1,25 +1,68 @@
 const questions = [
    
-        {
-          question: "What is the capital of Italy?",
-          options: ["Rome", "Milan", "Naples", "Turin"],
-          correctAnswer: "Rome",
-          image: "https://example.com/rome.jpg",
-        },
-        {
-          question: "Which Italian city is famous for its canals?",
-          options: ["Venice", "Florence", "Genoa", "Bologna"],
-          correctAnswer: "Venice",
-          image: "https://example.com/venice.jpg",
-        },
-        // Add more questions as needed
-     
+    {
+        question: "Which is my favourite color?",
+        options: ["True","False"],
+        correctAnswer: "True",
+        media: "../src/assets/red.PNG",
+        mediaType: "image",
+      },
+      {
+        question: "Which is more important for the italian economy?",
+        options: ["Pistacchio di Bronte","The Intel factory"],
+        correctAnswer: "Pistacchio di Bronte",
+        media: "../src/assets/pistacchio.jpg",
+        mediaType: "image",
+      },
+      {
+        question: "What did capitan Schettino do during the costa concordia accident?",
+        options: ["He accidentally fell into a lifeboat","He saved hundreds of people","He was busy meeting his misstress"],
+        correctAnswer: "He accidentally fell into a lifeboat",
+        media: "../src/assets/concordia.jpg",
+        mediaType: "image",
+      },
+      {
+        question: "Who is this gentleman?",
+        options: ["A femminist activist","Filippo Turati","A communist","A murderer who belongs in jail"],
+        correctAnswer: "He accidentally fell into a lifeboat",
+        media: "../src/assets/turetta.jpg",
+        mediaType: "image",
+      },
+      {
+        question: "What should you do?",
+        options: ["Contact the council to complain","Threaten the council and ask for pizzo", "Take the traffic light down and sell it for scrap copper"],
+        correctAnswer: "Take the traffic light down and sell it for scrap copper",
+        media: "Your local council has decided to install a new traffic light to ease the frequent traffic congestions in front of your home. After a few days you realize that the new hardware is making it difficult to drive at 200 mph without a license and plate with your 30 years old volkswagen golf 1.9 tdi",
+        mediaType: "text",
+      },
+      {
+        question: "Who is this guy?",
+        options: ["A true pillar of the community", "An innocent man", "A murderer who belongs in jail", "Idk"],
+        correctAnswer: "An innocent man",
+        media: "../src/assets/boss.PNG",
+        mediaType: "image",
+      },
+      {
+        question: "Watch this video, which is the best animal out there?",
+        options: ["Women", "Goats", "Vincitore becchi"],
+        correctAnswer: "Goats",
+        media: "https://www.youtube.com/embed/Jo07YIB3HBU", 
+        mediaType: "youtube",
+      },
+      {
+        question: "What should you do?",
+        options: ["Quando c'era lui i treni arrivavano in orario","You stand on the table and you do the famous handpose", "You scold him for making such distasteful jokes"],
+        correctAnswer: "You scold him for making such distasteful jokes",
+        media: "Your grandpa invites you to dinner and he starts yapping about the events from 1922 to 1943.",
+        mediaType: "text",
+      },
       
     {
         question: "Chi è?",
         options: ["A random bald italian man", "One of the greatest journalists", "The best", "idk"],
         correctAnswer: "One of the greatest journalists",
-        image: "../src/assets/benito.jpeg",
+        media: "../src/assets/benito.jpeg",
+        mediaType: "image"
       },
     // Add more questions as needed
   ];
